40b9aaf413504d71232614d1e54630ff574a2f4f
Ivan <Ivan@DESKTOP-FFSE4M7>
2017-01-12 14:41:34 -0800

    Add project files.

---
+++ WCFWebApp/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WCFWebApp
+{
+    public class Student
+    {
+        public int Id { set; get; }
+        public String Ime { get; set; }
+        public String Prezime { get; set; }
+    }
+}
---
+++ WCFWebApp/StudentService.svc.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using WCFWebApp;
+
+namespace Demo
+{
+    [ServiceContract(Namespace = "")]
+    [AspNetCompatibilityRequirements(RequirementsMode =
+    AspNetCompatibilityRequirementsMode.Allowed)]
+    public class StudentService
+    {
+        [OperationContract]
+        public Student GetStudentById(int studentId)
+        {
+            Student student = new Student();
+
+            string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("spGetStudentById", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter parameter = new SqlParameter();
+                parameter.ParameterName = "@Id";
+                parameter.Value = studentId;
+
+                cmd.Parameters.Add(parameter);
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    student.Id = Convert.ToInt32(rdr["Id"]);
+                    student.Ime = rdr["Ime"].ToString();
+                    student.Prezime = rdr["Prezime"].ToString();
+                }
+            }
+
+            return student;
+        }
+    }
+}
+
